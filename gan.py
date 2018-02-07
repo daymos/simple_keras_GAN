991f4cc96a1ea586e5a1bed89964f94debe99da0
daymos <matt.321null@gmail.com>
2018-02-07 16:22:19 +0000

    fix style

--- gan.py
+++ gan.py
@@ -1,5 +1,5 @@
-
-import sys
+# -*- coding: utf-8 -*-
+""" Simple implementation of Generative Adversarial Neural Network """
 
 import numpy as np
 
@@ -9,43 +9,38 @@ from keras.datasets import mnist
 from keras.layers import Input, Dense, Reshape, Flatten, Dropout
 from keras.layers import BatchNormalization
 from keras.layers.advanced_activations import LeakyReLU
-from keras.models import Sequential, Model
-from keras.optimizers import Adam, RMSprop
+from keras.models import Sequential
+from keras.optimizers import Adam
 
 import matplotlib.pyplot as plt
-plt.switch_backend('agg')
+plt.switch_backend('agg')   # allows code to run without a system DISPLAY
 
 
-#from sklearn.datasets import fetch_olivetti_faces
-#olivetti = fetch_olivetti_faces()
-
-#data = fetch_olivetti_faces().data  # images provided as array shaped (400, 4096) element -> each pic is 64x64
-        
 class GAN(object):
-    def __init__(self, width = 28, height= 28, channels = 1):
+    """ Generative Adversarial Network class """
+    def __init__(self, width=28, height=28, channels=1):
 
-        self.WIDTH = width
-        self.HEIGHT = height
-        self.CHANNELS = channels
+        self.width = width
+        self.height = height
+        self.channels = channels
 
-        self.SHAPE = (self.WIDTH, self.HEIGHT, self.CHANNELS)
+        self.shape = (self.width, self.height, self.channels)
 
-        self.OPTIMIZER = Adam(lr=0.0002, decay=8e-9)
+        self.optimizer = Adam(lr=0.0002, beta_1=0.5, decay=8e-8)
 
-        self.noise_gen = np.random.normal(0,1,(100,))
+        self.G = self.__generator()
+        self.G.compile(loss='binary_crossentropy', optimizer=self.optimizer)
 
-        self.G = self.generator()
-        self.G.compile(loss='binary_crossentropy', optimizer=self.OPTIMIZER)
+        self.D = self.__discriminator()
+        self.D.compile(loss='binary_crossentropy', optimizer=self.optimizer, metrics=['accuracy'])
 
-        self.D = self.discriminator()
-        self.D.compile(loss='binary_crossentropy', optimizer=self.OPTIMIZER, metrics=['accuracy'] )
+        self.stacked_generator_discriminator = self.__stacked_generator_discriminator()
 
-        self.stacked_G_D = self.stacked_G_D()
-        
-        self.stacked_G_D.compile(loss='binary_crossentropy', optimizer=self.OPTIMIZER)
+        self.stacked_generator_discriminator.compile(loss='binary_crossentropy', optimizer=self.optimizer)
 
 
-    def generator(self):
+    def __generator(self):
+        """ Declare generator """
 
         model = Sequential()
         model.add(Dense(256, input_shape=(100,)))
@@ -57,43 +52,44 @@ class GAN(object):
         model.add(Dense(1024))
         model.add(LeakyReLU(alpha=0.2))
         model.add(BatchNormalization(momentum=0.8))
-        model.add(Dense(self.WIDTH * self.HEIGHT * self.CHANNELS, activation='tanh'))
+        model.add(Dense(self.width  * self.height * self.channels, activation='tanh'))
+        model.add(Reshape((self.width, self.height, self.channels)))
 
-        model.add(Reshape((self.WIDTH, self.HEIGHT, self.CHANNELS)))
-        
         return model
 
-    def discriminator(self):
+    def __discriminator(self):
+        """ Declare discriminator """
 
         model = Sequential()
-        model.add(Flatten(input_shape=self.SHAPE))
-        model.add(Dense((self.WIDTH * self.HEIGHT * self.CHANNELS), input_shape=self.SHAPE))
+        model.add(Flatten(input_shape=self.shape))
+        model.add(Dense((self.width * self.height * self.channels), input_shape=self.shape))
         model.add(LeakyReLU(alpha=0.2))
-        model.add(Dense((self.WIDTH * self.HEIGHT * self.CHANNELS)/2))
+        model.add(Dense((self.width * self.height * self.channels)/2))
         model.add(LeakyReLU(alpha=0.2))
         model.add(Dense(1, activation='sigmoid'))
         model.summary()
 
         return model
 
-    def stacked_G_D(self):
-        self.D.trainable = False 
+    def __stacked_generator_discriminator(self):
+
+        self.D.trainable = False
 
         model = Sequential()
         model.add(self.G)
         model.add(self.D)
 
-        return model 
+        return model
 
-    def train(self, X_train, epochs=20000, batch = 32, save_interval = 200):
+    def train(self, X_train, epochs=20000, batch = 32, save_interval = 100):
 
         for cnt in range(epochs):
 
             ## train discriminator
-            random_index =  np.random.randint(0, len(X_train) - batch/2)
-            legit_images = X_train[random_index : random_index + batch/2].reshape(batch/2, self.WIDTH, self.HEIGHT, self.CHANNELS) 
+            random_index = np.random.randint(0, len(X_train) - batch/2)
+            legit_images = X_train[random_index : random_index + batch/2].reshape(batch/2, self.width, self.height, self.channels)
 
-            gen_noise = np.random.normal(0, 1, (batch/2,100))
+            gen_noise = np.random.normal(0, 1, (batch/2, 100))
             syntetic_images = self.G.predict(gen_noise)
 
             x_combined_batch = np.concatenate((legit_images, syntetic_images))
@@ -104,29 +100,30 @@ class GAN(object):
 
             # train generator
 
-            noise = np.random.normal(0, 1, (batch,100))
+            noise = np.random.normal(0, 1, (batch, 100))
             y_mislabled = np.ones((batch, 1))
 
-            g_loss = self.stacked_G_D.train_on_batch(noise, y_mislabled)
+            g_loss = self.stacked_generator_discriminator.train_on_batch(noise, y_mislabled)
 
             print ('epoch: %d, [Discriminator :: d_loss: %f], [ Generator :: loss: %f]' % (cnt, d_loss[0], g_loss))
-            
-            if cnt % save_interval == 0 : 
-                self.plot_images(save2file=True, step=cnt)
-        
 
-    def plot_images(self, save2file=False,  samples=16, step=0):
+            if cnt % save_interval == 0:
+                self.plot_images(save2file=True, step=cnt)
+
+
+    def plot_images(self, save2file=False, samples=16, step=0):
+        ''' Plot and generated images '''
         filename = "./images/mnist_%d.png" % step
-        noise = np.random.normal(0, 1, (samples,100))
+        noise = np.random.normal(0, 1, (samples, 100))
 
         images = self.G.predict(noise)
-        
-        plt.figure(figsize=(10,10))
-    
+
+        plt.figure(figsize=(10, 10))
+
         for i in range(images.shape[0]):
             plt.subplot(4, 4, i+1)
             image = images[i, :, :, :]
-            image = np.reshape(image, [ self.HEIGHT, self.WIDTH ])
+            image = np.reshape(image, [self.height, self.width])
             plt.imshow(image, cmap='gray')
             plt.axis('off')
         plt.tight_layout()
@@ -148,4 +145,3 @@ if __name__ == '__main__':
 
     gan = GAN()
     gan.train(X_train)
-
